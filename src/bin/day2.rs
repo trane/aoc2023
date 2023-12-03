efce6560d467b630c5d5c88ac48569790169d768
Andrew Kuhnhausen <andrew.kuhnhausen@calm.com>
2023-12-03 12:08:28 -0800

    day2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,3 +1,6 @@
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::io::Read;
 
 fn main() {
@@ -5,32 +8,95 @@ fn main() {
     std::io::stdin()
         .read_to_string(&mut input)
         .expect("Failed to read input");
-    let parsed = parse(&input);
-    let result = part1(&parsed);
-    println!("{}", result);
+    let parsed = parse_game(&input);
+    println!("{}", serde_json::to_string_pretty(&parsed).unwrap());
+    println!("part1: {}", part1(&parsed));
+    println!("part2: {}", part2(&parsed));
 }
 
-pub fn part1(parsed: &Vec<String>) -> i32 {
-    let mut result = 0;
-    for line in parsed {
-        let mut line_numbers: Vec<char> = Vec::new();
-        for character in line.chars() {
-            if character.is_numeric() {
-                line_numbers.push(character);
-            }
-        }
-        let mut num = String::new();
-        num += &line_numbers.first().unwrap().to_string();
-        num += &line_numbers.last().unwrap().to_string();
-        result += num.parse::<i32>().unwrap();
+pub fn parse_games(input: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    for line in input.lines() {
+        result.push(line.to_string());
     }
     return result;
 }
 
-pub fn parse(input: &str) -> Vec<String> {
-    let mut result = Vec::new();
+struct Bag {
+    blue: i32,
+    green: i32,
+    red: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Game {
+    number: i32,
+    blues: Vec<i32>,
+    greens: Vec<i32>,
+    reds: Vec<i32>,
+}
+
+fn parse_game(input: &str) -> Vec<Game> {
+    let re: Regex = Regex::new(r"^Game (\d+): (.+)$").unwrap();
+    let mut games = Vec::new();
     for line in input.lines() {
-        result.push(line.to_string());
+        match re.captures(line) {
+            Some(captures) => {
+                let game_number = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+                let game_reveals = captures.get(2).unwrap().as_str();
+                let mut game = Game {
+                    number: game_number,
+                    blues: Vec::new(),
+                    greens: Vec::new(),
+                    reds: Vec::new(),
+                };
+                for reveal in game_reveals.split("; ").collect::<Vec<&str>>() {
+                    for cubes in reveal.split(", ").collect::<Vec<&str>>() {
+                        let counts = cubes.split(" ").collect::<Vec<&str>>();
+                        let count = counts.first().unwrap().parse::<i32>().unwrap();
+                        let cube_color = counts.last().unwrap();
+                        match cube_color.to_string().as_str() {
+                            "blue" => game.blues.push(count),
+                            "green" => game.greens.push(count),
+                            "red" => game.reds.push(count),
+                            _ => panic!("Invalid cube color: {}", cube_color),
+                        }
+                    }
+                }
+                games.push(game)
+            }
+            None => panic!("Invalid input: {}", line),
+        }
+    }
+    return games;
+}
+
+fn part1(games: &Vec<Game>) -> i32 {
+    let bag = Bag {
+        blue: 14,
+        green: 13,
+        red: 12,
+    };
+    // let mut possible_games: Vec<Game> = Vec::new();
+    let mut result = 0;
+    for game in games {
+        let blue = game.blues.iter().max().unwrap();
+        let red = game.reds.iter().max().unwrap();
+        let green = game.greens.iter().max().unwrap();
+        if (bag.blue - blue) >= 0 && (bag.red - red) >= 0 && (bag.green - green) >= 0 {
+            result += game.number;
+        }
+    }
+    return result;
+}
+
+fn part2(games: &Vec<Game>) -> i32 {
+    let mut result = 0;
+    for game in games {
+        let blue = game.blues.iter().max().unwrap();
+        let red = game.reds.iter().max().unwrap();
+        let green = game.greens.iter().max().unwrap();
+        result += blue * red * green;
     }
     return result;
 }
