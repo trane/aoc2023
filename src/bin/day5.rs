3421e123945f0b64b2e24581f140ee8997927905
Andrew Kuhnhausen <andrew.kuhnhausen@calm.com>
2023-12-07 08:25:49 -0800

    day5 part2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,51 +1,42 @@
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use serde_json::{self, map, to_vec};
-use std::{
-    collections::{btree_map::Range, HashMap, HashSet},
-    io::Read,
-    result,
-};
+use std::io::Read;
 
 fn main() {
     let mut input = String::new();
     std::io::stdin()
         .read_to_string(&mut input)
         .expect("Failed to read input");
-    let maps = parse_lines(&input);
+    let (seeds, maps) = parse_lines(&input);
 
-    // println!("{}", serde_json::to_string_pretty(&cards).unwrap());
-    println!("part1: {}", part1(&maps));
-    // println!("part2: {}", part2(&cards));
+    println!("part1: {}", part1(&seeds, &maps));
+    println!("part2: {}", part2(&seeds, maps));
 }
 
-struct Input {
-    seeds: Vec<i32>,
-    maps: Vec<Map>,
-}
-
+#[derive(Serialize, Deserialize)]
 struct Map {
     name: String,
-    source_range: Vec<std::ops::Range<i32>>,
-    dest_range: Vec<std::ops::Range<i32>>,
+    source_range: Vec<std::ops::Range<i64>>,
+    dest_range: Vec<std::ops::Range<i64>>,
 }
 
-fn parse_lines(input: &str) -> Vec<Map> {
-    let mut result = Vec::new();
-    let seed_re = Regex::new(r"^seeds: (?<seeds>.+)$").unwrap();
-    let map_re = Regex::new(r"^(?<name>) map:$").unwrap();
+fn parse_lines(input: &str) -> (Vec<i64>, Vec<Map>) {
+    let mut results = Vec::new();
+    let mut seeds = Vec::new();
+    let seed_re = Regex::new(r"^seeds:\s+(?<seeds>.+)$").unwrap();
+    let map_re = Regex::new(r"^(?<name>.+)\s+map:$").unwrap();
     let map_values =
         Regex::new(r"^(?<dst_start>\d+)\s+(?<src_start>\d+)\s+(?<range>\d+)$").unwrap();
     for line in input.lines() {
         match seed_re.captures(line) {
             Some(captures) => {
-                let seeds = captures
+                seeds = captures
                     .name("seeds")
                     .unwrap()
                     .as_str()
                     .split_whitespace()
-                    .map(|x| x.parse::<i32>().unwrap())
-                    .collect::<Vec<i32>>();
+                    .map(|x| x.parse::<i64>().unwrap())
+                    .collect::<Vec<i64>>();
                 continue;
             }
             None => {}
@@ -55,7 +46,7 @@ fn parse_lines(input: &str) -> Vec<Map> {
                 let name = captures.name("name").unwrap().as_str().to_string();
                 let mut source_range = Vec::new();
                 let mut dest_range = Vec::new();
-                result.push(Map {
+                results.push(Map {
                     name: name,
                     source_range: source_range,
                     dest_range: dest_range,
@@ -70,52 +61,87 @@ fn parse_lines(input: &str) -> Vec<Map> {
                     .name("dst_start")
                     .unwrap()
                     .as_str()
-                    .parse::<i32>()
+                    .parse::<i64>()
                     .unwrap();
                 let src_start = captures
                     .name("src_start")
                     .unwrap()
                     .as_str()
-                    .parse::<i32>()
+                    .parse::<i64>()
                     .unwrap();
                 let range = captures
                     .name("range")
                     .unwrap()
                     .as_str()
-                    .parse::<i32>()
+                    .parse::<i64>()
                     .unwrap();
-                result[result.len() - 1]
-                    .source_range
-                    .push(src_start..(src_start + range));
-                result[result.len() - 1]
-                    .dest_range
-                    .push(dst_start..(dst_start + range));
+                results
+                    .last_mut()
+                    .map(|m| m.source_range.push(src_start..(src_start + range)));
+                results
+                    .last_mut()
+                    .map(|m| m.dest_range.push(dst_start..(dst_start + range)));
                 continue;
             }
             None => {}
         }
     }
-    return result;
+    return (seeds, results);
 }
 
-fn source_to_dest(map: Map, source: i32) -> i32 {
+fn source_to_dest(map: &Map, source: i64) -> i64 {
     for (pos, range) in map.source_range.iter().enumerate() {
         if range.contains(&source) {
-            return map.dest_range[pos].start + (source - range.start);
+            let value = map.dest_range[pos].start + (source - range.start);
+            return value;
         }
     }
-    return source;
+    return source.clone();
+}
+fn seed_to_location(seed: i64, maps: &Vec<Map>) -> i64 {
+    return maps.iter().fold(seed, |acc, map| source_to_dest(map, acc));
 }
 
-fn part1(input: &Input) -> i32 {
+fn part1(seeds: &Vec<i64>, maps: &Vec<Map>) -> i64 {
     let mut locations = Vec::new();
-    for seed in input.seeds {
-        let dest = seed;
-        for map in input.maps {
-            let dest = source_to_dest(map, seed);
-        }
-        locations.push(dest);
+    for seed in seeds {
+        locations.push(seed_to_location(*seed, maps));
     }
-    println!("{}", serde_json::to_string_pretty(&locations).unwrap());
+    // println!("{}", serde_json::to_string_pretty(&locations).unwrap());
     return locations.iter().min().unwrap().clone();
+}
+
+fn seed_ranges(seeds: &Vec<i64>) -> Vec<std::ops::Range<i64>> {
+    let mut ranges = Vec::new();
+    let mut i = 0;
+    while i < seeds.len() {
+        ranges.push(seeds[i]..(seeds[i] + seeds[i + 1]));
+        i += 2;
+    }
+    return ranges;
+}
+
+fn work(seed_range: std::ops::Range<i64>, maps: &Vec<Map>) -> i64 {
+    let mut lowest = i64::MAX;
+    for seed in seed_range {
+        let cur = seed_to_location(seed, maps);
+        if cur < lowest {
+            lowest = cur;
+        }
+    }
+    return lowest;
+}
+
+fn part2(seeds: &Vec<i64>, maps: Vec<Map>) -> i64 {
+    let seed_ranges = seed_ranges(seeds);
+    let mut lowest = i64::MAX;
+    for range in seed_ranges {
+        println!("starting range: {}..{}", range.start, range.end,);
+        let cur = work(range, &maps);
+        println!("...finished! cur: {}, lowest: {}", cur, lowest);
+        if cur < lowest {
+            lowest = cur;
+        }
+    }
+    return lowest;
 }
